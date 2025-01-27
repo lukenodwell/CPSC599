91bb35747340ae18d7747b1f92372250f85249af
Luke Nodwell <luke.nodwell@ucalgary.ca>
2025-01-26 18:35:37 -0700

    ProblemB Solved

--- Week2/ProblemB.py
+++ Week2/ProblemB.py
@@ -6,24 +6,26 @@ lines = input_data.splitlines()
 
 info = lines[0].split()
 
-questions = lines[1].split()
+questions = [int(q) for q in lines[1].split()]
 
 time_remaining = 300
 Num_AC = 0
 current_time = 0
 penalty_time = 0
 
-if (time_remaining - int(questions[int(info[1])]) >= 0):
-        time_remaining -= int(questions[int(info[1])])
-        current_time += int(questions[int(info[1])])
+if (time_remaining - questions[int(info[1])] >= 0):
+        time_remaining -= questions[int(info[1])]
+        current_time += questions[int(info[1])]
         penalty_time += current_time
         Num_AC += 1
         questions.pop(int(info[1]))
 
+        questions.sort()
+
         for question in questions:
                 if time_remaining - int(question) >= 0:
-                        time_remaining -= int(question)
-                        current_time += int(question)
+                        time_remaining -= question
+                        current_time += question
                         penalty_time += current_time
                         Num_AC += 1
 
--- Week2/ProblemE.py
+++ Week2/ProblemE.py
@@ -10,19 +10,10 @@ lines.pop(0)
 
 bdayNums = []
 
-jan = 31
-feb = 28
-mar = 31
-apr = 30
-may = 31
-jun = 30
-jul = 31
-aug = 31
-sep = 30
-oct = 31
-nov = 30
-dec = 31
+october28 = 302 # Added one for the date offset by 1
+
 start_of_month = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
+end_of_month = [31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
 
 # Convert all birthdays to a number
 for line in lines:
@@ -36,16 +27,45 @@ bdayNums.sort()
 
 # Find the largest difference between two birthdays
 largest_diff = 0
+largest_diff_index = 0
+time_to_october28 = float('inf')
 for i in range(1, len(bdayNums) + 1):
-        if i == len(bdayNums):
-                # print(bdayNums[i - 1])
-                # print(bdayNums[0])
-                diff = (365 - bdayNums[i - 1]) + bdayNums[0]
-                # print(diff)
+    if i == len(bdayNums):
+        diff = (365 - bdayNums[i - 1]) + bdayNums[0]
+        diff_index = bdayNums[0]
+    else:
+        diff = bdayNums[i] - bdayNums[i - 1]
+        diff_index = bdayNums[i]
+
+    if diff > largest_diff:
+        largest_diff = diff
+        largest_diff_index = diff_index
+        if largest_diff_index < october28:
+            time_to_october28 = (365 - october28) + diff_index
         else:
-                diff = bdayNums[i] - bdayNums[i - 1]
-        # print(diff)
-        if diff > largest_diff:
-                largest_diff = diff
-# print(largest_diff)
-print(bdayNums)
+            time_to_october28 = largest_diff_index - october28
+
+    elif diff == largest_diff:
+        if diff_index < october28:
+            temp_time_to_october28 = (365 - october28) + diff_index
+        else:
+            temp_time_to_october28 = diff_index - october28
+        if temp_time_to_october28 < time_to_october28:
+            largest_diff = diff
+            largest_diff_index = diff_index
+            time_to_october28 = temp_time_to_october28
+
+# Convert the largest_diff_index to a date
+for month in start_of_month:
+    if largest_diff_index < month:
+        month_index = start_of_month.index(month)
+        date_index = largest_diff_index - start_of_month[month_index - 1] - 1 # Offset by 1
+        if date_index == 0:
+            month_index -= 1
+            if month_index == 0:
+                month_index = 12
+            date_index = end_of_month[month_index - 1] - start_of_month[month_index - 1]
+        break
+
+formatted_date = f"{month_index:02d}-{date_index:02d}"
+print(formatted_date)
