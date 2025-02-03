c4b23083940dbcaf9ad738214f3b4411e4bcc9a0
Luke Nodwell <luke.nodwell@ucalgary.ca>
2025-02-02 22:45:27 -0700

    Edits to Problem B not working

--- Week3/ProblemB.py
+++ Week3/ProblemB.py
@@ -1,4 +1,5 @@
 import sys
+from decimal import Decimal, ROUND_HALF_UP
 
 input_data = sys.stdin.read()
 
@@ -7,25 +8,32 @@ lines = input_data.splitlines()
 # Number of cases
 n = int(lines[0])
 
-for n in range(1, n+1):
-        R, B, M = lines[n].split()
-        R = float(R)
-        B = float(B)
-        M = float(M)
-        R = R / 100
-        possible = True
+for i in range(1, n + 1):
+    R, B, M = lines[i].split()
+    R = Decimal(R) / 100
+    B = Decimal(B)
+    M = Decimal(M)
+    possible = True
 
-        payments = 0
-        while B > 0:
-                if payments == 1200:
-                        possible = False
-                        print("impossible")
-                        break
-                payments += 1
-                B += B * R
-                # print('Loan amount after interest before rounding', B)
-                B = round(B, 2)
-                # print('Loan amount after interest', B)
-                B -= M
-        if possible:
-                print(payments)
+    payments = 0
+    while B > 0:
+        if payments == 1200:
+            possible = False
+            print("impossible")
+            break
+        payments += 1
+        OG_B = B
+        B += B * R
+        # print('Loan amount after interest before rounding', B)
+        # Allow for rounding to the nearest 0.5 cents
+        B = B.quantize(Decimal('0.001'), rounding=ROUND_HALF_UP)
+        # print('Loan amount 3 digits', B)
+        # Then round to the nearest cent
+        B = B.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+        if OG_B - B > M:
+            print("impossible")
+            break
+        # print('Loan amount after interest', B)
+        B -= M
+    if possible:
+        print(payments)
