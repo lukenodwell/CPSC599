5b3eb90e3199ea5a74d7e6e35a5baf660fe05584
Luke Nodwell <luke.nodwell@ucalgary.ca>
2025-03-02 20:25:13 -0700

    Finished Problem B

--- Week6/ProblemB.py
+++ Week6/ProblemB.py
@@ -6,15 +6,27 @@ lines = input_data.splitlines()
 n = int(lines[0])
 cars = [int(lines[i]) for i in range(1, n + 1)]
 
-dp = [1] * n
+def longest_increasing_subsequence(arr):
+    lis = [1] * len(arr)
+    for i in range(len(arr) - 1, -1, -1):
+        for j in range(len(arr) - 1, i, -1):
+            if arr[i] < arr[j] and lis[i] < lis[j] + 1:
+                lis[i] = lis[j] + 1
+    return lis
 
+def longest_decreasing_subsequence(arr):
+    lds = [1] * len(arr)
+    for i in range(len(arr) - 1, -1, -1):
+        for j in range(len(arr) - 1, i, -1):
+            if arr[i] > arr[j] and lds[i] < lds[j] + 1:
+                lds[i] = lds[j] + 1
+    return lds
+
+lis = longest_increasing_subsequence(cars)
+lds = longest_decreasing_subsequence(cars)
+
+max_length = 0
 for i in range(n):
-    for j in range(i):
-        if cars[j] < cars[i]:
-            dp[i] = max(dp[i], dp[j] + 1)
+    max_length = max(max_length, lis[i] + lds[i] - 1)
 
-print(dp)
-
-longest_train_length = max(dp) if n > 0 else 0
-
-print(longest_train_length)
+print(max_length)
