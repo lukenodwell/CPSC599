b0b14d52f57a63c43399361c5b20c541fce4755c
Luke Nodwell <luke.nodwell@ucalgary.ca>
2025-04-06 22:37:22 -0600

    Finished Problem B

--- Week11/ProblemB.py
+++ Week11/ProblemB.py
@@ -1,64 +1,48 @@
 import sys
+from math import inf
 
-INF = 10**6
+input = sys.stdin.read
+data = input().split()
+idx = 0
 
-def floyd_warshall(n, edges, queries):
-    # Initialize distance matrix
-    dist = [[INF] * n for _ in range(n)]
+while True:
+    n, m, q = map(int, data[idx:idx+3])
+    idx += 3
+    if n == 0 and m == 0 and q == 0:
+        break
+
+    dist = [[inf] * n for _ in range(n)]
     for i in range(n):
         dist[i][i] = 0
 
-    # Populate initial distances from edges
-    for u, v, w in edges:
+    for _ in range(m):
+        u, v, w = map(int, data[idx:idx+3])
+        idx += 3
         dist[u][v] = min(dist[u][v], w)
 
-    # Floyd-Warshall algorithm
     for k in range(n):
         for i in range(n):
             for j in range(n):
-                if dist[i][k] < INF and dist[k][j] < INF:
+                if dist[i][k] < inf and dist[k][j] < inf:
                     dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
-    # Detect negative cycles
     for k in range(n):
         if dist[k][k] < 0:
             for i in range(n):
                 for j in range(n):
-                    if dist[i][k] < INF and dist[k][j] < INF:
-                        dist[i][j] = -INF
+                    if dist[i][k] < inf and dist[k][j] < inf:
+                        dist[i][j] = -inf
 
-    # Process queries
     results = []
-    for u, v in queries:
-        if dist[u][v] == INF:
+    for _ in range(q):
+        u, v = map(int, data[idx:idx+2])
+        idx += 2
+        if dist[u][v] == inf:
             results.append("Impossible")
-        elif dist[u][v] == -INF:
+        elif dist[u][v] == -inf:
             results.append("-Infinity")
         else:
             results.append(str(dist[u][v]))
-    return results
-
-input = sys.stdin.read
-data = input().splitlines()
-idx = 0
-
-while idx < len(data):
-        n, m, q = map(int, data[idx].split())
-        if n == 0:
-                break
-        idx += 1
-
-        edges = []
-        for _ in range(m):
-                u, v, w = map(int, data[idx].split())
-                edges.append((u, v, w))
-                idx += 1
-
-queries = []
-for _ in range(q):
-        u, v = map(int, data[idx].split())
-        queries.append((u, v))
-        idx += 1
-
-results = floyd_warshall(n, edges, queries)
-sys.stdout.write("\n".join(results) + "\n\n")
+        
+    print("\n".join(results))
+    print()
